98ae71a39bfc75a847d54c76a8947a266adce078
Nana-Kwame-A <kwameandw@gmail.com>
2022-12-12 15:23:34 -0800

    third task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,12 +9,10 @@ int main(void)
 {
 	char c;
 
-	c = 'a';
-	while
-		(c <= '2') {
-			putchar(c);
-			c++;
-		}
+	for (c = 'a'; c <= '2'; c++)
+	{
+		putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
