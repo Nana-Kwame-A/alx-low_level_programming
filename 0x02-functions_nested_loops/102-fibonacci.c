2481440853d43029ba24a33ec55ac7d6d119d3bf
Nana-Kwame-A <kwameandw@gmail.com>
2022-12-14 16:54:59 -0800

    Tasks

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,62 +1,28 @@
 #include <stdio.h>
-
 /**
- * numLength - returns the length of a string
- * @num: operand number
- * Return: number of digits
- */
-
-int numLength(int num)
-{
-	int length = 0;
-
-	if (!num)
-	{
-		return (1);
-	}
-
-	while (num)
-	{
-		num = num / 10;
-		length += 1;
-	}
-	return (length);
-}
-
-/**
- * *main - prints the first 98 fibonaci sequences
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
-
 int main(void)
 {
-	unsigned long f1 = 1, f2 = 2, tmp, mx = 100000000, f10 = 0, f20 = 0, tmpo = 0;
-	short int i = 1, initial0s;
+	int i = 0;
+	long int a = 0, b = 1, next;
 
-	while (i <= 98)
+	while (i < 50)
 	{
-		if (f10 > 0)
-			printf("%lu", f10);
-		initial0s = numLength(mx) - 1 - numLength(f1);
-		while (f10 > 0 && initial0s > 0)
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 49)
 		{
-			printf("%1u", 0);
-				initial0s--;
-		}
-		printf("%lu", f1);
-
-		tmp = (f1 + f2) % mx;
-		tmpo = f10 + f20 + (f1 + f2) / mx;
-		f1 = f2;
-		f10 = f20;
-		f2 = tmp;
-		f20 = tmpo;
-
-		if (i != 98)
 			printf(", ");
-		else
-			printf("\n");
+		}
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
