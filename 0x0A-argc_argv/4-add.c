b21fc2af5fcb8a7f536e2939554c1e8a3aed007b
Nana-Kwame-A <kwameandw@gmail.com>
2023-01-09 15:34:43 -0800

    0x0A. C - argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -7,9 +7,9 @@
 	 * check_num - check - string there are digit
 	 * @str: array str
 	 * Return: Always 0 (Success)
-	 */
+	*/
 
-	int check_num(char *str)
+int check_num(char *str)
 	{
 		/*Declaring variables*/
 		unsigned int count;
@@ -36,7 +36,7 @@
 	 * Return: Always 0 (Success)
 	 */
 
-	int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 
 	{
 
