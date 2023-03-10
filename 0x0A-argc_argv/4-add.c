75c84f59062fcaeb3a7181eb3bf2115206bf79f6
Nana-Kwame-A <kwameandw@gmail.com>
2023-03-10 06:02:21 -0800

    Tasks

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,11 +3,11 @@
 #include <ctype.h>
 #include <string.h>
 
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 * Return: Always 0 (Success)
-	*/
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ * Return: Always 0 (Success)
+*/
 
 int check_num(char *str)
 	{
@@ -28,13 +28,13 @@ int check_num(char *str)
 		return (1);
 	}
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+*/
 
 int main(int argc, char *argv[])
 
