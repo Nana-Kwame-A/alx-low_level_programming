a222f3dd7eb9d13bc98741c36bfa8d2642abbbba
Nana-Kwame-A <kwameandw@gmail.com>
2023-05-26 17:44:40 -0700

    updated tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	v = write(fo, text_content, len);
 
-	if (fo == -1 || w == -1)
+	if (fo == -1 || v == -1)
 		return (-1);
 
 	close(fo);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -22,7 +22,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	p = open(filename, O_WRONLY | O_APPEND);
 	v = write(o, text_content, len);
 
-	if (p == -1 || w == -1)
+	if (p == -1 || v == -1)
 		return (-1);
 
 	close(p);
